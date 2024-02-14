0597fdd89a529a9e3af2a322a7212eed33759dcb
nour7mohamed <norhanhassan107@gmail.com>
2024-02-14 23:53:03 +0200

    nour

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str =  “The Zen of Python”
- “The Zen of Python” = The Zen of Python, by Tim Peters
+ “The Zen of Python” = "The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -20,6 +20,6 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
+Namespaces are one honking great idea -- let's do more of those!"
 
 ptint( str)
